572e4908f3c7e63116b03e27835b53ad9775f6aa
Marchenkoan <machen@inbox.ru>
2020-05-18 18:07:09 +0300

    added department

--- storeroom/models.py
+++ storeroom/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+###
+### Отдел
+class Department(models.Model):
+    department = models.CharField(max_length=5, name='Отдел', db_index=True)
+    slug = models.SlugField(max_length=5)
+
+    def __str__(self):
+        return self.department
+
